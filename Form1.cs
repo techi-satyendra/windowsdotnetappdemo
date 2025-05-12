725c93024258c050b09d9950081ca81670f46df7
Satyendra <Satyendra@DESKTOP-J8F3B1T>
2025-05-12 21:56:19 +0530

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windowsdotnetappdemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hello Satyendra,how are you?");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "test,successfull.";
+        }
+    }
+}
